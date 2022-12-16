7fc78dda57d939994fbc54c5db5b0531a29cad26
Val-Bal <100865492+Val-Bal@users.noreply.github.com>
2022-12-16 23:49:22 +0100

    created five Pokemon objects and added them to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 let pokemonList = [];
+
+pokemonList.push(
+    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
+    { name: "Charmander", height: 6, types: ['fire'] },
+    { name: "Squirtle", height: 5, types: ['water'] },
+    { name: "Pikachu", height: 4, types: ['electric'] },
+    { name: "Dratini", height: 8, types: ['dragon'] },
+);
+
