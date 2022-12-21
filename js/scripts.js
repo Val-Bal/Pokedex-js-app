3164af518a9870e2f408d27ece24b946f27e54f8
Val-Bal <100865492+Val-Bal@users.noreply.github.com>
2022-12-21 19:49:05 +0100

    corrected the values for the pokemon's height

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,27 @@ let pokemonList = [];
 pokemonList.push(
     {
         name: "Bulbasaur",
-        height: 7,
+        height: 0.7,
         types: ['grass', 'poison']
     },
     {
         name: "Charmander",
-        height: 6,
+        height: 0.6,
         types: ['fire']
     },
     {
         name: "Squirtle",
-        height: 5,
+        height: 0.5,
         types: ['water']
     },
     {
         name: "Pikachu",
-        height: 4,
+        height: 0.4,
         types: ['electric']
     },
     {
         name: "Dratini",
-        height: 8,
+        height: 1.8,
         types: ['dragon']
     },
 );
