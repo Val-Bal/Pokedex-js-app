37927c5bf55721e30879c66f3ca76310cc1f5f03
Val-Bal <100865492+Val-Bal@users.noreply.github.com>
2022-12-21 19:38:57 +0100

    make pokemonList array more readable

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 let pokemonList = [];
 
 pokemonList.push(
-    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
-    { name: "Charmander", height: 6, types: ['fire'] },
-    { name: "Squirtle", height: 5, types: ['water'] },
-    { name: "Pikachu", height: 4, types: ['electric'] },
-    { name: "Dratini", height: 8, types: ['dragon'] },
+    {
+        name: "Bulbasaur",
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: "Charmander",
+        height: 6,
+        types: ['fire']
+    },
+    {
+        name: "Squirtle",
+        height: 5,
+        types: ['water']
+    },
+    {
+        name: "Pikachu",
+        height: 4,
+        types: ['electric']
+    },
+    {
+        name: "Dratini",
+        height: 8,
+        types: ['dragon']
+    },
 );
 
 for (let i = 0; i < pokemonList.length; i++) {
