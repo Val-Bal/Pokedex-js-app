a1f34543b275de21acf4152217b7e3de35ffb73c
Val-Bal <100865492+Val-Bal@users.noreply.github.com>
2023-01-10 10:49:33 +0100

    added comments

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,7 @@ let pokemonRepository = (function () {
         */
 })();
 
-// add Pokemon to array 
+// add Pokemon to array with add() 
 pokemonRepository.add(
     {
         name: "Bulbasaur",
@@ -101,7 +101,7 @@ pokemonList.push(
 );
 */
 
-//return pokemonList through pokemonRepository
+//return pokemonList through pokemonRepository, don't forget the () to call function
 pokemonRepository.getAll().forEach(function (pokemon) {
     document.write("<p>" + pokemon.name + " (height: " + pokemon.height + "m)");
 
@@ -122,21 +122,4 @@ pokemonList.forEach(function (pokemon) {
         document.write(" - Wow, that’s big!" + "</p>");
     }
 });
-*/
-
-/*
-1. In the “scripts.js” file of your project, wrap your pokemonList array in an IIFE to avoid accidentally accessing the global state.
-
-2. Before anything else, create a new pokemonRepository variable to hold what your IIFE will return, then assign the IIFE to that variable.
-
-3. The IIFE should return an object with the following public functions assigned as keys:
-- getAll: return all items (pokemonRepository.getAll(); should return the pokemonList array)
-- add: add a single item to the pokemonList array (calling pokemonRepository.add(item); should add the Pokémon referred to with item to the pokemonList array)
-
-4. Make sure both functions are defined separately with the function keyword. Also, the IIFE returns only an object with the same names for keys as values (see the last paragraph of the Exercise)
-
-5. Outside of and below the IIFE, you should already have a forEach() loop that iterates over each Pokémon in the repository. 
-But since you’ve limited access to the pokemonList array that’s inside the IIFE (so that it’s only accessible through one of the two functions returned by the IIFE), 
-you need to update the loop code to cope with the new changes. 
-Essentially, you need to use one of the two functions returned by the IIFE in order to retrieve the pokemonList array.
 */
