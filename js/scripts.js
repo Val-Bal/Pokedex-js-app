f5d5694ad3341e7988f72aa0ee87ac8cfa6285d0
Val-Bal <100865492+Val-Bal@users.noreply.github.com>
2023-02-16 23:27:44 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -84,27 +84,4 @@ pokemonRepository.loadList().then(function () {
     });
 });
 
-
-/*
-Bonus Task A1.5
-
-Want even more practice? You may have noticed that the add() function lets you add anything to pokemonList within the repository. 
-You can even add strings or numbers. That’s not good. 
-In a real application, you’d want to make sure pokemonList can only be modified with the correct type of data. 
-Inside the addv function, you can check if the typeof parameter is an object. 
-In combination with a conditional, make sure you can only add the passed argument of the function to pokemonList if it’s an object.
-
-In addition to the type, you can also validate whether all Object.keys() of the parameter are equal to the specific keys you expect.
-
-If you’re feeling truly adventurous, you can take a look at how the filter() function works and 
-create a whole new public function for pokemonRepository that allows you to find specific Pokémon only by name.
-*/
-
-/*
-Bonus Task A1.7
-Display a loading message while data is being loaded. To do this, implement a showLoadingMessage() and hideLoadingMessage() function, 
-which appends/removes a message to your page. Hint #1: showLoadingMessage() should be the first thing executed inside both the LoadList() 
-and loadDetails() functions. Hint #2: hideLoadingMessage() should be executed in the then and catch blocks of the fetch code of both 
-LoadList() and loadDetails(). This makes sense because once you’re inside then or catch, it means you’ve received the response from 
-the fetch code and can hide the loading message.
-*/
+console.log(pokemonRepository);
